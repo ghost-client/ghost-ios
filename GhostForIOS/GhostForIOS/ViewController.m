7763f681f99615e78f421518a126e3e8bb5594cf
hangzhang <15038777234@163.com>
2015-03-18 11:10:29 +0800

    初始化IOS工程

---
+++ GhostForIOS/GhostForIOS/ViewController.m
@@ -0,0 +1,31 @@
+//
+//  ViewController.m
+//  GhostForIOS
+//
+//  Created by hangzhang on 15/3/18.
+//  Copyright (c) 2015 IOS版GHOST. All rights reserved.
+//
+
+
+#import "ViewController.h"
+
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # ghost-ios
 GHOST mobile client for IOS
+
+###这个版本为IOS版本的GHOST
+
+---
+#作者:[君赏](http://js.uiapple.com)
